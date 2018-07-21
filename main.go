19351ed719d85f5db202e1412cd7b349da2b81a9
Thomas Obenaus <obenaus.thomas@gmail.com>
2018-07-21 22:19:44 +0200

    .Add added dependency to aws-sdk

--- main.go
+++ main.go
@@ -2,8 +2,30 @@ package main
 
 import (
 	"log"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 func main() {
 	log.Println("Hello world")
+
+	sess := session.Must(session.NewSession(&aws.Config{Region: aws.String("us-west-2"),
+		Credentials: credentials.NewSharedCredentials("", "playground")}))
+
+	//(&aws.Config{
+	//	Region:      aws.String("us-west-2"),
+	//	Credentials: credentials.NewSharedCredentials("", "test-account"),
+	//})
+
+	ss3Ep := s3.New(sess)
+	buckets, err := ss3Ep.ListBuckets(nil)
+	if err != nil {
+		log.Fatalf("Error: %s", err.Error())
+	}
+
+	log.Printf("Buckerts: %s", buckets)
+
 }
