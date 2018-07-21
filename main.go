b2119d9a55c419da7cab23a07a6a0fd49e55fe2c
Thomas Obenaus <obenaus.thomas@gmail.com>
2018-07-21 19:20:40 +0200

    .Add added testify dep

--- main.go
+++ main.go
@@ -2,13 +2,8 @@ package main
 
 import (
 	"log"
-
-	terraform "github.com/hashicorp/terraform/terraform"
 )
 
 func main() {
 	log.Println("Hello world")
-	state := terraform.State{}
-
-	log.Printf("State %s", state)
 }
